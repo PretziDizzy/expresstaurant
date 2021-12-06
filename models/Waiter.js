e22d09f5096dfe573c2de57a1639302b60e1c52c
Preeti Das <preetidas_a@yahoo.com>
2021-12-06 16:06:08 -0600

    modified attribute names

--- models/Waiter.js
+++ models/Waiter.js
@@ -4,8 +4,8 @@ const {DataTypes, Model} = require('sequelize')
 class Waiter extends Model {}
 
 Waiter.init({
-    Waiter_name: DataTypes.STRING,
-    Waiter_salary: DataTypes.INTEGER,
+    waiter_name: DataTypes.STRING,
+    waiter_salary: DataTypes.INTEGER,
     
 }, {
     sequelize,
