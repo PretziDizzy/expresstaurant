473ff1e27e31bd461cfa118165da0a0a8a023407
Preeti Das <preetidas_a@yahoo.com>
2021-12-06 15:38:12 -0600

    made changes

--- seed.js
+++ seed.js
@@ -1,34 +1,25 @@
 const {sequelize} = require('./db')
 //const {Restaurant, Menu, Item} = require('./models/index') //Q: WHY import these models from index vs. from each separate model file?
-const {Restaurant} = require('./models/Restaurant')
+const {Waiter} = require('./models/Waiter')
 
 //Q: Why do you think each object inside of the arrays are structured the way that they are?
 //Q: What do you think will happen when we 'seed' this file?
-const seedRestaurant = [
+const seedWaiter = [
   {
-    name: 'AppleBees',
-    location: 'Texas',
-    cuisine: 'FastFood'
+    waiter_name: 'James',
+    waiter_salary: '700',
   },
   {
-    name: 'LittleSheep',
-    location: 'Dallas',
-    cuisine: 'Hotpot'
+    waiter_name: 'Cathy',
+    waiter_salary: '1000',
   },
   {
-    name: 'Spice Grill',
-    location: 'Houston',
-    cuisine: 'Indian'
+    waiter_name: 'Jimmy',
+    waiter_salary: '800',
   },
   {
-    name: 'burgetking',
-    location: 'Dallas',
-    cuisine: 'Hotpot'
-  },
-  {
-    name: 'MCDS',
-    location: 'Dallas',
-    cuisine: 'Hotpot'
+    waiter_name: 'James',
+    waiter_salary: '700',
   },
 ]
 
@@ -76,7 +67,7 @@ const seedRestaurant = [
 const seed = async () => {
   try {
     await sequelize.sync({force: true})
-    await Restaurant.bulkCreate(seedRestaurant, {validate: true})
+    await Waiter.bulkCreate(seedWaiter, {validate: true})
     // await Menu.bulkCreate(seedMenu, {validate: true})
     // await Item.bulkCreate(seedItem, {validate: true})
     console.log('Seeding success!')
--- server.js
+++ server.js
@@ -1,30 +1,30 @@
 const express = require('express')
 const path = require('path') //node native module
-const { Restaurant } = require('./models/Restaurant')
+const { Waiter } = require('./models/Waiter')
 
 const app = express()
 const port = 3000
 
 //points toward folder of static files
-app.use(express.static(path.join(__dirname, 'public')))
+// app.use(express.static(path.join(__dirname, 'public')))
 
 //GET method on /flipcoin route responds with heads or tails
-app.get('/flipcoin', (req, res) => {
-    let coinflip = Math.floor(Math.random()*2)
-    if (coinflip == 1){
-        coinflip = 'Heads'
-    } else {
-        coinflip = 'Tails'
-    }
-    res.send(coinflip)
-})
+// app.get('./Waiter', (req, res) => {
+//     // let coinflip = Math.floor(Math.random()*2)
+//     // if (coinflip == 1){
+//     //     coinflip = 'Heads'
+//     // } else {
+//     //     coinflip = 'Tails'
+//     // }
+//     // res.send(coinflip)
+// })
 
 //GET method on /restaurants route returns all restaurants
-app.get('/restaurants', async (req,res) => {
+app.get('/waiter', async (req,res) => {
     //find all instances of the Model Restaurant
-    const allRestaurants = await Restaurant.findAll()
+    const allWaiters = await Waiters.findAll()
     //respond with allRestaurants as a json objeect
-    res.json(allRestaurants)
+    res.json(allWaiters)
 })
 
 app.listen(port, () => {
