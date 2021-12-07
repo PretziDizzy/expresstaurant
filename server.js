7cb9f624e6515e14fb9f42f3b5e286cf3783532f
Preeti Das <preetidas_a@yahoo.com>
2021-12-07 15:48:59 -0600

    created some routes

--- server.js
+++ server.js
@@ -20,13 +20,45 @@ app.use(express.static(path.join(__dirname, 'public')))
 // })
 
 //GET method on /restaurants route returns all restaurants
-app.get('/waiter', async (req,res) => {
+// app.get('/waiter/:name', async (req,res) => {
+//     //find all instances of the Model Restaurant
+//     const thiswaiter = await Waiter.findOne({ where:{waiter_name:req.params.name }})
+//     console.log(req)
+//     //respond with allRestaurants as a json objeect
+//     res.json(thiswaiter)
+// })
+// app.get('/waiter', async (req,res) => {
+//     //find all instances of the Model Restaurant
+//     const thiswaiters = await Waiter.findAll()
+//     console.log(req)
+//     //respond with allRestaurants as a json objeect
+//     res.json(thiswaiters)
+// })
+
+// app.get('/waiters/:id', async (req,res) => {
+//     //find all instances of the Model Restaurant
+//     const onewaiter = await Waiter.findByPk(req.params.id)
+//     console.log(req)
+//     //respond with allRestaurants as a json objeect
+//     res.json(onewaiter)
+// })
+
+app.get('/search', async (req,res) => {
     //find all instances of the Model Restaurant
-    const allWaiters = await Waiter.findAll()
-    console.log(req)
+    let results = []
+    if (req.query.name) {
+        results = await Waiter.findAll({where: {waiter_name: req.query.name}})
+    }
+    else if (req.query.salary) {
+        results = await Waiter.findAll({where: {waiter_salary: req.query.salary}})
+    }
     //respond with allRestaurants as a json objeect
-    res.json(allWaiters)
+    res.json(results)
 })
+// run a query in postman: http://localhost:3000/search?name=James
+
+
+
 
 app.listen(port, () => {
     console.log(`Server listening at http://localhost:${port}`)
